d238a3a76fdfc744caecc418152a18148a11b72d
Alyson Diego Fagundes dos Santos <87543879+AlysonfSantos@users.noreply.github.com>
2022-05-18 16:03:37 -0300

    Get - recebendo parâmetros de busca.

--- ProjetoLoja.Api/Controllers/ProdutoController.cs
+++ ProjetoLoja.Api/Controllers/ProdutoController.cs
@@ -19,13 +19,29 @@ namespace ProjetoLoja.Api.Controllers
         public async Task<IActionResult> ListarProdutos()
         {
             var produtos = await _produtoAppService.ListarProdutos();
+            if (produtos == null) return NotFound("Nenhum produto encontrado");
             return Ok(produtos);
         }
+        [HttpGet]
+        [Route("{nome}")]
+        public async Task<IActionResult> ListarProdutos(string nome)
+        {
+            var produtos = await _produtoAppService.ListarProdutos();
+            if (produtos == null) return NotFound("Nenhum produto encontrado");
+            foreach (var prod in produtos) 
+            {
+                if (prod.Nome == nome)
+                    return Ok(prod);
+            }
+            return Ok();
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> CadastrarProduto([FromBody] NovoProdutoViewModel vm)
         {
             var result = await _produtoAppService.CadastrarProduto(vm);
+            if (result == null) return BadRequest("Não foi possível cadastrar o produto");
             return Ok(result);
         }
 
@@ -34,6 +50,7 @@ namespace ProjetoLoja.Api.Controllers
         public  async Task<IActionResult> AtualizarProduto([FromBody] AtualizarProdutoViewModel vm)
         {
             var result = await _produtoAppService.AtualizarProduto(vm);
+            if (result == null) return BadRequest("Não foi possível Atualizar o produto");
             return Ok(result);
         }
 
@@ -43,6 +60,7 @@ namespace ProjetoLoja.Api.Controllers
         public async Task<IActionResult> DeletarProduto(long id) 
         {
             var result = await _produtoAppService.DeletarProduto(id);
+            if (!result) return BadRequest($"Não foi possível excluir produto {id}");
             if (result) return Ok();
             return NotFound();
         }
